227a61bd18f51fc81aad301511536d7582375975
Henry Welle <henry.welle@icloud.com>
2026-09-17 20:42:00 -0400

    add scout rank as first rank in dropdown

--- lib/scoutList_screen.dart
+++ lib/scoutList_screen.dart
@@ -38,6 +38,7 @@ class _ScoutListScreenState extends State<ScoutListScreen> {
                 });
               },
               items: <String>[
+                'Scout',
                 'Tenderfoot',
                 'Second Class',
                 'First Class',
